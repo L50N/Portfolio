e64620a3c369c038a0993e183e2391708a45de94
Leon <92170476+L50N@users.noreply.github.com>
2024-10-14 17:33:13 +0200

    + Note added to the imprint

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Hier entsteht ein Portfolio!</h1>
+<p>Laut dem Digitale-Dienste-Gesetz (DDG) wird für rein private Websites, die ausschließlich persönlichen oder familiären Zwecken dienen, kein Impressum benötigt.</p>
