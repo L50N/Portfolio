74aea6ad65c2fcd85d074e6c605f1b36d3e21728
Leon <92170476+L50N@users.noreply.github.com>
2024-10-15 00:42:59 +0200

    + Fixed that the SvelteJs adapter is not found and Tailwindcss is not retrieved in the layout.

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,7 @@
+<div class="flex flex-row space-x-4">
+	<ul class="list-none">
+		<li>
+			<a href="/impressum">Impressum</a>
+		</li>
+	</ul>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
 <h1>Hier entsteht ein Portfolio!</h1>
 <p>Laut dem Digitale-Dienste-Gesetz (DDG) wird für rein private Websites, die ausschließlich persönlichen oder familiären Zwecken dienen, kein Impressum benötigt.</p>
+
+<script>
+	import Footer from '$lib/components/Footer.svelte';
+</script>
+
+<div>
+	<Footer />
+</div>
