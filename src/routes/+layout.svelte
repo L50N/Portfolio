e376caef37ecc6c8dc0e7f7dd6fc6d622a2a98db
Leon <92170476+L50N@users.noreply.github.com>
2024-10-15 11:44:38 +0200

    + A new font, tailwindcss support imported in the layout.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,14 @@
-<div>
-    <slot />
+<script>
+	import "../app.css";
+</script>
+
+<div class="min-h-screen pt-[3.05rem] md:pt-[3.25rem] font-instrument font-bold flex items-center flex-col px-8 sm:px-18 md:px-0 overflow-x-hidden p-0 m-0">
+	<!-- In diesem Slot wird der Inhalt der Seite eingebettet. -->
+	<div class="md:w-[72vw] md:max-w-[104rem] flex-grow w-full mx-auto">
+		<div class="flex flex-grow">
+			<div class="w-full">
+				<slot />
+			</div>
+		</div>
+	</div>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        'instrument': ['"Instrument Sans"', 'sans-serif'],
+      },
+      keyframes: {
+        slideUp: {
+          '0%': { bottom: '-250px' },
+          '100%': { bottom: '2rem' },
+        },
+        slideDown: {
+          '0%': { bottom: '2rem' },
+          '100%': { bottom: '-250px' },
+        },
+      },
+      animation: {
+        slideUp: 'slideUp 1s forwards',
+        slideDown: 'slideDown 2s forwards',
+      },
+      height: {
+        'hero-calc': 'calc(100vh - 3.25rem)',
+      },
+      screens: {
+        'nsp': '336px',
+        'xs': '416px',
+        '2.5xl': '1968px',
+        '3xl': '2168px',
+        '4xl': '2468px',
+        'hero': '3168px',
+      }
+    },
   },
-  plugins: []
-};
+  plugins: [],
+}
