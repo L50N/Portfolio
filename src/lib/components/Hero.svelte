afdf936b18d1beafe88d372496c98d93f4f1476e
Leon <92170476+L50N@users.noreply.github.com>
2024-10-15 16:40:12 +0200

    + Hero with image and improved styles

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,17 +1,26 @@
-<div class="grid grid-cols-2 w-full space-x-12">
-	<div id="right" class="flex items-start flex-col justify-end">
-		<img class="select-none h-10 w-10 object-cover rounded-full" src="/Profile.png" alt="">
-		<div class="py-2 leading-3">
-			<h1 class="text-2xl">Leon Bartnik</h1>
-			<p class="font-light text-[#6B6B6B]">I develop & design software and websites.</p>
+<div class="space-y-6 mt-12">
+	<div class="grid grid-cols-2 w-full space-x-12">
+		<div id="right" class="flex items-start flex-col justify-end">
+			<img class="select-none h-10 w-10 object-cover rounded-full" src="/Profile.png" alt="">
+			<div class="mt-2 leading-2">
+				<h1 class="text-2xl line-clamp-1">Leon Bartnik</h1>
+				<p class="font-light text-[#6B6B6B] line-clamp-1">I develop & design software and websites.</p>
+			</div>
+		</div>
+		<div id="left" class="flex items-end flex-col justify-end">
+			<div class="flex flex-row items-center space-x-1">
+				<p class="font-medium text-[#585858] text-[0.89rem] line-clamp-1">Scroll to find out more</p>
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="size-4 select-none">
+					<path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25 12 21m0 0-3.75-3.75M12 21V3" />
+				</svg>
+			</div>
 		</div>
 	</div>
-	<div id="left" class="flex items-end flex-col justify-end">
-		<div class="flex flex-row items-center space-x-1">
-			<p class="font-light text-[#585858] text-[0.89rem]">Scroll to find out more</p>
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="size-4 select-none">
-				<path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25 12 21m0 0-3.75-3.75M12 21V3" />
-			</svg>
+	<div class="w-full">
+		<div
+			class="mt-4 select-none relative w-full h-[52.64vw] sm:h-[12.85rem] lg:h-[14.89rem] xl:h-[18.16rem] 2xl:h-[19.58rem] transition-all delay-100 ease-in-out">
+			<img src="/IMG_0252.JPEG"
+					 class="grayscale w-full h-full rounded-2xl 2xl:rounded-[1.45rem] object-cover" alt="">
 		</div>
 	</div>
 </div>
--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -7,15 +7,15 @@
 	<div class="space-y-4">
 		<div>
 			<a href="/" class="inline-block">
-				<button class="flex justify-center items-center h-10 w-10 rounded-full bg-[#E8E8E8]">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="#545454" class="size-5">
+				<button class="flex justify-center items-center h-9 w-9 rounded-full bg-[#E8E8E8]">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.05" stroke="#545454" class="size-[1.1rem]">
 						<path stroke-linecap="round" stroke-linejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
 					</svg>
 				</button>
 			</a>
 		</div>
 		<div class="space-y-2 lg:w-[42rem]">
-			<h1 class="text-[1.3925rem] leading-7 xs:leading-none xs:text-2xl lg:text-[1.75rem]">{$page.status}
+			<h1 class="font-semibold text-[1.3925rem] leading-7 xs:leading-none xs:text-2xl lg:text-[1.75rem]">{$page.status}
 				{$page.error.message} ─ We were unable to access this page on our server at the current time.
 			</h1>
 			<p class="text-[#7A7A7A] font-light leading-[1.1725rem] lg:leading-5 text-[0.885rem] lg:text-[0.925rem]">
@@ -27,7 +27,7 @@
 		<div
 			class="mt-4 relative w-full h-[52.25vw] sm:h-[15.75rem] lg:h-[18.25rem] xl:h-[22.25rem] 2xl:h-[24rem] transition-all delay-100 ease-in-out">
 			<img src="/IMG_0252.JPEG"
-					 class="grayscale w-full h-full rounded-xl lg:rounded-2xl 2xl:rounded-[1.25rem] object-cover" alt="">
+					 class="grayscale w-full h-full rounded-2xl 2xl:rounded-[1.45rem] object-cover" alt="">
 		</div>
 	</div>
 </div>
