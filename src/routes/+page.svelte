cbaf564842a33599c42911420091eb75525353c2
Leon <92170476+L50N@users.noreply.github.com>
2024-10-15 12:35:19 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import Hero from "$lib/components/Hero.svelte";
 </script>
 
-<h1 class="text-2xl">Hier entsteht ein Portfolio!</h1>
+<h1 class="text-2xl">A portfolio is being built here!</h1>
 <p class="font-light">Laut dem Digitale-Dienste-Gesetz (DDG) wird für rein private Websites, die ausschließlich persönlichen oder familiären Zwecken dienen, kein Impressum benötigt.</p>
 
 <div>
